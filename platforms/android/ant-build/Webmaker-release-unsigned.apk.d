e06c7d843119c9f492542c7b72c0c146f30dd33e
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-12-10 16:36:09 -0800

    Initial attempt at CI for Android

---
+++ platforms/android/ant-build/Webmaker-release-unsigned.apk.d
@@ -0,0 +1,5 @@
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/Webmaker-release-unsigned.apk : \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/Webmaker.ap_ \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes.dex \
+/Users/asliwinski/Source/beaker/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/asliwinski/Source/beaker/platforms/android/libs/jetty-websocket-8.1.16.v20140903.jar \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,16 @@
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes.dex : \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket$Close.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket$Creation$1.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket$Creation.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket$JettyWebSocket.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket$Send.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/com/knowledgecode/cordova/websocket/WebSocket.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/BuildConfig.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/R$attr.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/R$drawable.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/R$string.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/R$xml.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/R.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/classes/org/mozilla/beaker/Webmaker.class \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/dexedLibs/classes-ce33a6d923c826b3ca4a294df68e13cc.jar \
+/Users/asliwinski/Source/beaker/platforms/android/ant-build/dexedLibs/jetty-websocket-8.1.16.v20140903-45f8dea684ed2420ce4e95ffff5d8403.jar \
