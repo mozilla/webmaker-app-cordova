9e1e8149f79d552a2e920665029c772a0c4f6ca6
Kate Hudson <k88hudson@gmail.com>
2015-01-08 16:25:39 -0500

    figuring out contacts, iframe communication

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,23 +1,41 @@
 var app = {
   initialize: function() {
     var req = new XMLHttpRequest;
-
     req.onload = function(e) {
       var jsonResponse = JSON.parse(req.responseText);
       var iframe = document.getElementById('app-iframe');
       iframe.src = jsonResponse.appUrl;
       iframe.setAttribute('mozapp', jsonResponse.appManifestUrl);
+
+      var pingpong = new Pingpong({
+        remoteOrigin: jsonResponse.appUrl,
+        remoteWindow: iframe.contentWindow
+      });
+
+      pingpong.ping();
+      
     }
 
     req.open('GET', 'config.json', true);
     req.send();
-  }    
+  },
+  connect: function () {
+
+  }
 };
 
 // Init on window load
 window.addEventListener('load', function () {
     FastClick.attach(document.body);
     app.initialize();
+}, false);
+
+document.addEventListener('deviceready', function () {
+  navigator.contacts.find(['*'], function (contacts) {
+    console.log(contacts);
+  }, function (err) {
+    console.log(err);
+  });
 }, false);
 
 // "Active" styles hack for mobile webkit
