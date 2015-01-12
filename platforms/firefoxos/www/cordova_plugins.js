76f7daa5cca5840ec26fd480672b324a2b3c4289
Kate Hudson <k88hudson@gmail.com>
2015-01-12 18:09:01 -0500

    all files

--- platforms/firefoxos/www/cordova_plugins.js
+++ platforms/firefoxos/www/cordova_plugins.js
@@ -2,8 +2,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.knowledgecode.cordova.websocket": "0.7.0"
-}
+{}
 // BOTTOM OF METADATA
 });
